b919ea6a0e1c9e8402813be4c09a08ff165f78db
Arya siddarath rao <96486950+Aryasiddarathrao@users.noreply.github.com>
2022-05-25 22:44:33 +0530

    Create neon_number_using_func.py

---
+++ neon_number_using_func.py
@@ -0,0 +1,16 @@
+def neon(num):
+    sum=0
+    num**=2
+    while num>0:
+        rem=num%10
+        sum+=rem
+        num//=10
+    return sum
+num=int(input('enter the number'))
+temp=num
+result=neon(num)
+if result==temp:
+    print('neon')
+else:
+    print('not neon')
+© 2022 GitHub, Inc.
